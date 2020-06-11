306b969d187df56339b9038f4382259585d40532
ERogalla <erogalla@utexas.edu>
2020-06-10 21:04:20 -0500

    displaying changing number

--- app.py
+++ app.py
@@ -4,12 +4,23 @@ import streamlit as st
 import matplotlib.pyplot as plt
 from keras.datasets import mnist
 
-st.title('My App')
-
-(trainX, trainy), (testX, testy) = mnist.load_data()
-
-plt.imshow(trainX[0], cmap=pyplot.get_cmp('gray'))
-
-st.pyplot()
+import time
 
 
+def main():
+    st.title('My App')
+
+    (trainX, trainy), (testX, testy) = mnist.load_data()
+
+    number = st.empty()
+    for i in range(9):
+        shownumber(trainX, i)
+        number.pyplot()
+        time.sleep(1)
+
+def shownumber(trainX, i):
+    plt.show(300)
+    plt.imshow(trainX[i], cmap=plt.get_cmap('gray'))
+
+if __name__ == "__main__":
+    main()
